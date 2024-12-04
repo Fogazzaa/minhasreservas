a0c11525bba6520f4619b86170bb31f264476e00
fogazza <vinicius.cintra2801@gmail.com>
2024-12-04 14:10:48 -0300

    04-12-24

---
+++ main/back-end/src/db/connect.js
@@ -0,0 +1,12 @@
+const mysql = require("mysql2");
+
+
+const pool = mysql.createPool({
+    connectionLimit:10,
+    host:'10.89.240.84',
+    user:'maju',
+    password: '1234',
+    database: 'rs'
+})
+
+module.exports = pool;
